5f9fe55889abd24ed32befac99a8b972c5e3edb9
piyo <chick.p.1120@gmail.com>
2023-01-08 12:09:30 +0900

    feat(bookmeter2slack): add

---
+++ scripts/bookmeter2slack/Code.ts
@@ -0,0 +1,117 @@
+const BOOKMETER_USER_ID =
+  PropertiesService.getScriptProperties().getProperty("BOOKMETER_USER_ID") ||
+  "";
+const SLACK_WEBHOOK_URL =
+  PropertiesService.getScriptProperties().getProperty("SLACK_WEBHOOK_URL") ||
+  "";
+
+type BookmeterResource = {
+  id: number;
+  created_at: string;
+  book: {
+    id: number;
+    title: string;
+    image_url: string;
+    author: {
+      name: string;
+    };
+    page: number;
+  };
+  content: string;
+};
+
+type BookmeterResponse = {
+  resources: BookmeterResource[];
+};
+
+type SlackAttachment = {
+  color?: string;
+  fields: Record<"title" | "value", string>[];
+};
+
+const fetchReadBooks_ = (userguide: string): BookmeterResponse => {
+  const url = `https://bookmeter.com/users/${userguide}/books/read.json?sort=read_at&order=desc&offset=0&limit=10`;
+  const response = UrlFetchApp.fetch(url);
+  const json: BookmeterResponse = JSON.parse(response.getContentText());
+  return json;
+};
+
+const filterReadBooksYesterday_ = (
+  response: BookmeterResponse
+): BookmeterResource[] => {
+  const dt = new Date();
+  dt.setDate(dt.getDate() - 1);
+
+  const resources = response.resources || [];
+  return resources.filter((resource) => {
+    return (
+      Utilities.formatDate(dt, "JST", "yyyy/MM/dd") === resource.created_at
+    );
+  });
+};
+
+const determineColorCode_ = (): string => {
+  return `#${Math.random().toString(16).slice(-6)}`;
+};
+
+const postSlack_ = ({
+  books,
+  webhookUrl,
+}: {
+  books: BookmeterResource[];
+  webhookUrl: string;
+}): boolean => {
+  const attachments = books.reduce<SlackAttachment[]>((acc, resource) => {
+    const data = {
+      color: determineColorCode_(),
+      fields: [
+        {
+          title: "タイトル",
+          value: resource.book.title,
+        },
+        {
+          title: "著者",
+          value: resource.book.author.name,
+        },
+        {
+          title: "感想",
+          value: resource.content,
+        },
+      ],
+    };
+    return [...acc, data];
+  }, []);
+
+  const body = {
+    pretext: "読んだ本",
+    attachments: attachments,
+  };
+  const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+    method: "post",
+    contentType: "application/json",
+    payload: JSON.stringify(body),
+  };
+  const response = UrlFetchApp.fetch(webhookUrl, options);
+  return response.getResponseCode() == 200;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function main(): void {
+  if (BOOKMETER_USER_ID) {
+    throw new Error("Required bookmater account");
+  }
+  if (SLACK_WEBHOOK_URL) {
+    throw new Error("Required slack webhook url");
+  }
+
+  const books = fetchReadBooks_(BOOKMETER_USER_ID);
+  const yesterdayBooks = filterReadBooksYesterday_(books);
+  if (yesterdayBooks.length == 0) {
+    return;
+  }
+
+  // Slack
+  postSlack_({ books: yesterdayBooks, webhookUrl: SLACK_WEBHOOK_URL })
+    ? "Success"
+    : "Failed";
+}
---
+++ scripts/bookmeter2slack/README.md
@@ -0,0 +1,33 @@
+# bookmeter2slack
+
+This is a google apps script that posts to Slack about the books which read last night.
+
+## Usage
+
+1. Login to your Google account.
+
+   ```shell
+   $ npm run login
+   ```
+
+1. Create a new google apps project.
+
+   ```shell
+   $ cd scripts/bookmeter2slack
+   $ npm run create
+   $ npm run push
+   ```
+
+1. Setup script propeties for required paramters.
+
+   1. Access to the project page.
+
+      ```shell
+      $ npm run open
+      ```
+
+   1. Open [Project Settings].
+   1. Add the below propeties.
+
+      - `BOOKMETER_USER_ID`: Bookmeter ID
+      - `SLACK_WEBHOOK_URL`: Slack Webbhook URL
