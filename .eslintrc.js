c8dbffe1bcef4801fcbab0824c3023a4fbd889e9
piyo <chick.p.1120@gmail.com>
2023-01-08 11:25:06 +0900

    chore: setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+};
