58d7b02073c6378c34dfc79032eeb2e35a4e6050
piyo <chick.p.1120@gmail.com>
2024-04-06 09:28:03 +0900

    chore: migrate flatconfig

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
+import typeScriptESLintParser from "@typescript-eslint/parser";
+
+const compat = new FlatCompat();
+const recommended = js.configs;
+
+export default [
+  {
+    recommended,
+    eslintConfigPrettier,
+    ...compat.extends("plugin:@typescript-eslint/eslint-recommended"),
+    parser: typeScriptESLintParser,
+    ignores: ["**/node_modules/", "**/build/*", "**/testing/*"],
+  },
+];
